1b790dfd41a0259ebbbfe86c37b358a5105d5df3
Caroline <2015302@aluno.univesp.br>
2022-07-17 20:04:22 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header"
 import SalesCard from "./components/SalesCard"
 
@@ -5,6 +7,7 @@ function App() {
   return (
 
     <>
+    <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
