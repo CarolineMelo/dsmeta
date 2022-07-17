0ba56d6b8acaafc12b49e103f95d78fe0b53bc55
Caroline <2015302@aluno.univesp.br>
2022-07-17 19:58:07 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -69,13 +69,13 @@ function SalesCard() {
                                 <tr key={sale.id}>
                                     <td className="show992">{sale.id}</td>
                                     <td className="show576">{new Date (sale.date).toLocaleDateString()}</td>
-                                    <td>{sale.sellerName}</td>
+                                    <td> {sale.sellerName}</td>
                                     <td className="show992">{sale.visited}</td>
                                     <td className="show992">{sale.deals}</td>
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id}/>
                                         </div>
                                     </td>
                                 </tr>
